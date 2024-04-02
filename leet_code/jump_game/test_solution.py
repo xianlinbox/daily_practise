5579aef563624d1ea8712f63938cb603226a3dcc
xianning liu <xianlinbox@gmail.com>
2024-04-02 08:54:34 -0500

    added test cases

--- leet_code/jump_game/test_solution.py
+++ leet_code/jump_game/test_solution.py
@@ -6,7 +6,10 @@ class TestJump(unittest.TestCase):
     solution = Solution()
 
     def test_case_1(self):
-        pass
+        self.assertTrue(self.solution.canJump([2, 3, 1, 1, 4]))
+
+    def test_case_2(self):
+        self.assertTrue(self.solution.canJump([3, 2, 1, 0, 4]))
 
 
 if __name__ == "__main__":
