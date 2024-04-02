42e58d095eb6a2d1c3a4f6e1ffa6b9bbae67e4ec
xianning liu <xianlinbox@gmail.com>
2024-04-02 08:47:49 -0500

    add jump problem

---
+++ leet_code/jump_game/test_solution.py
@@ -0,0 +1,13 @@
+import unittest
+from solution import Solution
+
+
+class TestJump(unittest.TestCase):
+    solution = Solution()
+
+    def test_case_1(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
