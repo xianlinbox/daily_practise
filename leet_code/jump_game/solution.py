a0c4ac91377f810e206b891871181156d9f4654b
xianning liu <xianlinbox@gmail.com>
2024-04-03 08:51:48 -0500

    added solution for jump

--- leet_code/jump_game/solution.py
+++ leet_code/jump_game/solution.py
@@ -3,4 +3,7 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        pass
+        for i in range(0, len(nums)):
+            if nums[i] == len(nums) - (i + 1):
+                return True
+        return False
--- leet_code/jump_game/test_solution.py
+++ leet_code/jump_game/test_solution.py
@@ -9,7 +9,13 @@ class TestJump(unittest.TestCase):
         self.assertTrue(self.solution.canJump([2, 3, 1, 1, 4]))
 
     def test_case_2(self):
-        self.assertTrue(self.solution.canJump([3, 2, 1, 0, 4]))
+        self.assertFalse(self.solution.canJump([3, 2, 1, 0, 4]))
+
+    def test_case_3(self):
+        self.assertTrue(self.solution.canJump([0]))
+
+    def test_case_4(self):
+        self.assertFalse(self.solution.canJump([3]))
 
 
 if __name__ == "__main__":
