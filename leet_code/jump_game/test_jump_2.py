3b7a1a3de3b51f4dbb81ef8c764b287a1efe63c1
xianning liu <xianlinbox@gmail.com>
2024-04-06 00:03:37 -0500

    add more jump 2 test cases

--- leet_code/jump_game/test_jump_2.py
+++ leet_code/jump_game/test_jump_2.py
@@ -11,6 +11,12 @@ class TestJump(unittest.TestCase):
     def test_case_2(self):
         self.assertEqual(2, self.solution.jump([3, 2, 1, 0, 4]))
 
+    def test_case_3(self):
+        self.assertEqual(1, self.solution.jump([5, 2, 1, 0, 4]))
+
+    def test_case_4(self):
+        self.assertEqual(3, self.solution.jump([1, 3, 1, 4, 1, 1, 1]))
+
 
 if __name__ == "__main__":
     unittest.main()
