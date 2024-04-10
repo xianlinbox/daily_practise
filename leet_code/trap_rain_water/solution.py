3a243e0db38d54bd588f2478a5d1a53cbdf2fe1a
xianning liu <xianlinbox@gmail.com>
2024-04-09 20:28:28 -0500

    add first solution

--- leet_code/trap_rain_water/solution.py
+++ leet_code/trap_rain_water/solution.py
@@ -3,4 +3,20 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        pass
+        left_max_heights = []
+        current_left_max = 0
+        for each_height in height:
+            current_left_max = max(current_left_max, each_height)
+            left_max_heights.append(current_left_max)
+
+        height.reverse()
+        right_max_heights = []
+        current_right_max = 0
+        for reverse_height in height:
+            current_right_max = max(current_right_max, reverse_height)
+            right_max_heights.append(current_left_max)
+        result = 0
+        for i in range(0, len(height)):
+            result += min(left_max_heights[i], right_max_heights[i]) - height[i]
+
+        return result
