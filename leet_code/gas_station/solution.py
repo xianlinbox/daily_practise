b4f621d5e9fd75fdc93b7b6451fab7e923ad3a1d
xianning liu <xianlinbox@gmail.com>
2024-04-11 20:59:29 -0500

    add first solution

--- leet_code/gas_station/solution.py
+++ leet_code/gas_station/solution.py
@@ -3,6 +3,14 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        remain_gas = 0
         for i in range(0, len(gas)):
-            remain_gas = gas[i]
-            remain_gas -= cost[i]
+            remain_gas = gas[i] - cost[i]
+            j = i
+            while remain_gas > 0:
+                j += 1
+                remain_gas += gas[j % len(gas)] - cost[j % len(gas)]
+                if j == i:
+                    return i
+
+        return -1
