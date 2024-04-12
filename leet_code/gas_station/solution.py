8b3eb170702ca3a2a5daba04c19356f4859d9dc9
xianning liu <xianlinbox@gmail.com>
2024-04-11 20:50:25 -0500

    update test cases

--- leet_code/gas_station/solution.py
+++ leet_code/gas_station/solution.py
@@ -3,4 +3,6 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        pass
+        for i in range(0, len(gas)):
+            remain_gas = gas[i]
+            remain_gas -= cost[i]
--- leet_code/gas_station/test_solution.py
+++ leet_code/gas_station/test_solution.py
@@ -8,16 +8,20 @@ class TestJump(unittest.TestCase):
     def test_case_1(self):
         self.assertEqual(
             3,
-            self.solution.canCompleteCircuit(
-                [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], [3, 4, 5, 1, 2]
-            ),
+            self.solution.canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]),
         )
 
-    def test_case_2(self):
+    def test_case_1(self):
         self.assertEqual(
             -1,
             self.solution.canCompleteCircuit([2, 3, 4], [3, 4, 3]),
         )
+
+    # def test_case_2(self):
+    #     self.assertEqual(
+    #         -1,
+    #         self.solution.canCompleteCircuit([2, 3, 4], [3, 4, 3]),
+    #     )
 
 
 if __name__ == "__main__":
