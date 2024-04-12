dbef23cc0e262288c5871ba8465ed7fc6e13aa28
xianning liu <xianlinbox@gmail.com>
2024-04-12 10:49:54 -0500

    fix the 0 scenario

--- leet_code/gas_station/solution.py
+++ leet_code/gas_station/solution.py
@@ -4,13 +4,13 @@ from typing import List
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         remain_gas = 0
-        for i in range(0, len(gas)):
+        station_count = len(gas)
+        for i in range(0, station_count):
             remain_gas = gas[i] - cost[i]
             j = i
-            while remain_gas > 0:
-                print(i, j)
-                j += 1
-                remain_gas += gas[j % len(gas)] - cost[j % len(gas)]
+            while remain_gas >= 0:
+                j = (j + 1) % station_count
+                remain_gas += gas[j] - cost[j]
                 if j == i:
                     return i
 
