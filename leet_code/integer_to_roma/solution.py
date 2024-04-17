944d10707b6876ba9fe3a3edfc2c24f5413ee149
xianning liu <xianlinbox@gmail.com>
2024-04-16 23:09:27 -0500

    handle 2 digits

--- leet_code/integer_to_roma/solution.py
+++ leet_code/integer_to_roma/solution.py
@@ -3,17 +3,22 @@ from typing import List
 
 class Solution:
     def intToRoman(self, num: int) -> str:
+        rules = [
+            ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+            ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+        ]
+
+        #         I             1
+        # V             5
+        # X             10
+        # L             50
+        # C             100
+        # D             500
+        # M             1000
+
         digits = list(str(num))
         digits.reverse()
         result = ""
         for i in range(0, len(digits)):
-            digit = int(digits[i])
-            match i:
-                case 0:
-                    if digit < 5:
-                        result += "I" * digit
-                    elif digit == 5:
-                        result += "V"
-                    else:
-                        result += "V" + (digit - 5) * "I"
+            result = rules[i][int(digits[i]) - 1] + result
         return result
--- leet_code/integer_to_roma/test_solution.py
+++ leet_code/integer_to_roma/test_solution.py
@@ -11,11 +11,20 @@ class TestJump(unittest.TestCase):
     def test_case_0_1(self):
         self.assertEqual("V", self.solution.intToRoman(5))
 
-    def test_case_0_1(self):
-        self.assertEqual("VIIII", self.solution.intToRoman(9))
+    def test_case_0_2(self):
+        self.assertEqual("IX", self.solution.intToRoman(9))
 
-    # def test_case_1(self):
-    #     self.assertEqual("LVIII", self.solution.intToRoman(58))
+    def test_case_0_3(self):
+        self.assertEqual("IV", self.solution.intToRoman(4))
+
+    def test_case_1(self):
+        self.assertEqual("LVIII", self.solution.intToRoman(58))
+
+    def test_case_1_1(self):
+        self.assertEqual("XXXVIII", self.solution.intToRoman(38))
+
+    def test_case_1_2(self):
+        self.assertEqual("LXVIII", self.solution.intToRoman(68))
 
     # def test_case_2(self):
     #     self.assertEqual("MCMXCIV", self.solution.intToRoman(1994))
