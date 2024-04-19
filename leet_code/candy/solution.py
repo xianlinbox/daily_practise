5751c2ee3a0ba2012eb7c4dae197ede362ea9b5c
xianning liu <xianlinbox@gmail.com>
2024-04-18 20:43:40 -0500

    add test case 3

--- leet_code/candy/solution.py
+++ leet_code/candy/solution.py
@@ -4,7 +4,6 @@ from typing import List
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         candies = [1] * len(ratings)
-
         for i in range(0, len(ratings)):
             if i > 0:
                 if ratings[i] > ratings[i - 1]:
--- leet_code/candy/test_solution.py
+++ leet_code/candy/test_solution.py
@@ -19,6 +19,14 @@ class TestJump(unittest.TestCase):
         result = self.solution.candy([1, 1, 1])
         self.assertEqual(3, result)
 
+    def test_case_3(self):
+        result = self.solution.candy([1, 1, 1])
+        self.assertEqual(3, result)
+
+    def test_case_4(self):
+        result = self.solution.candy([1, 1, 1])
+        self.assertEqual(3, result)
+
 
 if __name__ == "__main__":
     unittest.main()
