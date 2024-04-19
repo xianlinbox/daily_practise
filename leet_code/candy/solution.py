210d6704b5fc83cf984e609e123e205bb6c25c50
xianning liu <xianlinbox@gmail.com>
2024-04-18 20:41:26 -0500

    fix index issue

--- leet_code/candy/solution.py
+++ leet_code/candy/solution.py
@@ -3,11 +3,12 @@ from typing import List
 
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        candies = []
+        candies = [1] * len(ratings)
 
         for i in range(0, len(ratings)):
-            candies[i] = 1
-            if i > 0 and ratings[i] > ratings[i - 1]:
-                candies[i] = candies[i - 1] + 1
-
+            if i > 0:
+                if ratings[i] > ratings[i - 1]:
+                    candies[i] = candies[i - 1] + 1
+                elif ratings[i] < ratings[i - 1]:
+                    candies[i - 1] = candies[i] + 1
         return sum(candies)
