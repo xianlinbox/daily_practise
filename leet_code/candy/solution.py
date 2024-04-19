a6147904565dd391d36f3c526aea6c42829f0568
xianning liu <xianlinbox@gmail.com>
2024-04-18 20:37:07 -0500

    add initial solution

--- leet_code/candy/solution.py
+++ leet_code/candy/solution.py
@@ -3,4 +3,11 @@ from typing import List
 
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        pass
+        candies = []
+
+        for i in range(0, len(ratings)):
+            candies[i] = 1
+            if i > 0 and ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        return sum(candies)
