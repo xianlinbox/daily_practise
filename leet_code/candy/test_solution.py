a844a924fbe11aa60ace68008dbab10e445ca07b
xianning liu <xianlinbox@gmail.com>
2024-04-18 20:46:18 -0500

    add test case 4

--- leet_code/candy/test_solution.py
+++ leet_code/candy/test_solution.py
@@ -20,12 +20,12 @@ class TestJump(unittest.TestCase):
         self.assertEqual(3, result)
 
     def test_case_3(self):
-        result = self.solution.candy([1, 1, 1])
-        self.assertEqual(3, result)
+        result = self.solution.candy([1, 2, 3, 3, 5, 5])
+        self.assertEqual(10, result)
 
     def test_case_4(self):
-        result = self.solution.candy([1, 1, 1])
-        self.assertEqual(3, result)
+        result = self.solution.candy([1, 2, 87, 87, 87, 2, 1])
+        self.assertEqual(13, result)
 
 
 if __name__ == "__main__":
