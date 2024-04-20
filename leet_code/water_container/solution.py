259f22849c178f23e4906a09990a185a6682b98c
xianning liu <xianlinbox@gmail.com>
2024-04-20 12:06:25 -0500

    use 2 pointers to reduce running time

--- leet_code/water_container/solution.py
+++ leet_code/water_container/solution.py
@@ -3,11 +3,15 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        max_area = 0
-        for i in range(0, len(height)):
-            for j in range(i + 1, len(height)):
-                h = min(height[i], height[j])
-                if h * (j - i) > max_area:
-                    max_area = h * (j - i)
+        i = 0
+        j = len(height) - 1
+        max_area = min(height[i], height[j]) * (j - i)
+        while i < j:
+            if height[i] > height[j]:
+                j -= 1
+            else:
+                i += 1
+            if min(height[i], height[j]) * (j - i) > max_area:
+                max_area = min(height[i], height[j]) * (j - i)
 
         return max_area
