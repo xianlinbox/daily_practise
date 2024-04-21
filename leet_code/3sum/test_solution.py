f2fec4cb81633762ce9f7246a91bb261e771b45b
xianning liu <xianlinbox@gmail.com>
2024-04-21 10:41:16 -0500

    add general test cases

--- leet_code/3sum/test_solution.py
+++ leet_code/3sum/test_solution.py
@@ -6,8 +6,16 @@ class TestJump(unittest.TestCase):
     solution = Solution()
 
     def test_case_0(self):
-        result = self.solution.max([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])
-        self.assertEqual(3, result)
+        result = self.solution.threeSum([-1, 0, 1, 2, -1, -4])
+        self.assertEqual([[-1, -1, 2], [-1, 0, 1]], result)
+
+    def test_case_1(self):
+        result = self.solution.threeSum([0, 1, 1])
+        self.assertEqual([], result)
+
+    def test_case_2(self):
+        result = self.solution.threeSum([0, 0, 0])
+        self.assertEqual([0, 0, 0], result)
 
 
 if __name__ == "__main__":
