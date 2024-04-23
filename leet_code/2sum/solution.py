7505d0bf05e90d082da3e49ef487ccde854bd6a8
xianning liu <xianlinbox@gmail.com>
2024-04-23 13:49:07 -0500

    add 2sum solution

--- leet_code/2sum/solution.py
+++ leet_code/2sum/solution.py
@@ -3,4 +3,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        pass
+        i = 0
+        j = len(numbers) - 1
+        while True:
+            if numbers[i] + numbers[j] == target:
+                return [i + 1, j + 1]
+            elif numbers[i] + numbers[j] > target:
+                j = j - 1
+            else:
+                i = i + 1
--- leet_code/2sum/test_solution.py
+++ leet_code/2sum/test_solution.py
@@ -7,15 +7,15 @@ class TestJump(unittest.TestCase):
 
     def test_case_0(self):
         result = self.solution.twoSum([2, 7, 11, 15], 9)
-        self.assertEqual([[2, 7]], result)
+        self.assertEqual([1, 2], result)
 
     def test_case_1(self):
         result = self.solution.twoSum([2, 3, 4], 6)
-        self.assertEqual([[1, 3]], result)
+        self.assertEqual([1, 3], result)
 
     def test_case_2(self):
         result = self.solution.twoSum([-1, 0], -1)
-        self.assertEqual([[1, 2]], result)
+        self.assertEqual([1, 2], result)
 
 
 if __name__ == "__main__":
