40c723d398850db578b999c39d5a9b4908244034
xianning liu <xianlinbox@gmail.com>
2024-04-24 08:53:10 -0500

    3sum performance tunning

--- leet_code/3sum/solution.py
+++ leet_code/3sum/solution.py
@@ -5,21 +5,31 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         result = []
         nums.sort()
-        for index, item in enumerate(nums):
-            target = 0 - item
-            i = index + 1
-            j = len(nums) - 1
-            while True:
-                if i >= j:
-                    break
-                if nums[i] + nums[j] == target:
-                    if [nums[index], nums[i], nums[j]] not in result:
-                        result.append([nums[index], nums[i], nums[j]])
-                    i = i + 1
-                    j = j - 1
-                elif nums[i] + nums[j] > target:
-                    j = j - 1
+        n = len(nums)
+
+        for first in range(n):
+            if nums[first] > 0:
+                break
+            if first > 0 and nums[first] == nums[first - 1]:
+                continue
+
+            target = 0 - nums[first]
+            i = first + 1
+            j = n - 1
+
+            while i < j:
+                total = nums[i] + nums[j]
+                if total == target:
+                    result.append([nums[first], nums[i], nums[j]])
+                    while i < j and nums[i] == nums[i + 1]:
+                        i += 1
+                    while i < j and nums[j] == nums[j - 1]:
+                        j -= 1
+                    i += 1
+                    j -= 1
+                elif total < target:
+                    i += 1
                 else:
-                    i = i + 1
+                    j -= 1
 
         return result
