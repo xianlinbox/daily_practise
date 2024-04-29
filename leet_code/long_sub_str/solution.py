3ba144c5bef7d2cfd6b95b26c8c63f07f68b98b1
xianning liu <xianlinbox@gmail.com>
2024-04-29 08:22:15 -0500

    performance tunning round 1

--- leet_code/long_sub_str/solution.py
+++ leet_code/long_sub_str/solution.py
@@ -9,10 +9,10 @@ class Solution:
         left = 0
         result_len = 0
         for right in range(n):
-            while str_array[right] in result:
-                print(left, right, result)
+            curr_item = str_array[right]
+            while curr_item in result:
                 result.remove(str_array[left])
                 left += 1
-            result.append(str_array[right])
-            result_len = max(result_len, len(result))
+            result.append(curr_item)
+            result_len = max(result_len, right - left + 1)
         return result_len
