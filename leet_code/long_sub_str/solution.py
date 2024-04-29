ac6fe2a599a6a278c3f06b7787c1f68682643b27
xianning liu <xianlinbox@gmail.com>
2024-04-29 07:56:16 -0500

    finish solution

--- leet_code/long_sub_str/solution.py
+++ leet_code/long_sub_str/solution.py
@@ -3,16 +3,16 @@ from typing import List
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        result = []
-        left = 0
         str_array = list(s)
         n = len(str_array)
+        result = []
+        left = 0
         result_len = 0
         for right in range(n):
-            result.append(str_array[right])
-            while str_array[left] in result:
+            while str_array[right] in result:
                 print(left, right, result)
                 result.remove(str_array[left])
                 left += 1
+            result.append(str_array[right])
             result_len = max(result_len, len(result))
         return result_len
