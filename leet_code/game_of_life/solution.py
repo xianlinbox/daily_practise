d9b44da509091410b28064675d84e0fc1530715e
xianning liu <xianlinbox@gmail.com>
2024-05-03 16:25:48 -0500

    first solution

--- leet_code/game_of_life/solution.py
+++ leet_code/game_of_life/solution.py
@@ -4,17 +4,26 @@ from typing import List
 # https://leetcode.com/problems/game-of-life/description/?envType=study-plan-v2&envId=top-interview-150
 class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
-        for row in range(len(board)):
-            for column in range(len(board[0])):
-                item = board[row][column]
-                surround_lives = self.calculate_surround_lives(board, row, column)
+        rows = len(board)
+        columns = len(board[0])
+        surround_lives_board = [[0 for _ in range(columns)] for _ in range(rows)]
+        for row in range(rows):
+            for column in range(columns):
+                surround_lives_board[row][column] = self.calculate_surround_lives(
+                    board, row, column
+                )
 
+        for row in range(rows):
+            for column in range(columns):
+                surround_lives = surround_lives_board[row][column]
                 if surround_lives < 2 or surround_lives > 3:
                     board[row][column] = 0
                 elif surround_lives == 3:
                     board[row][column] = 1
 
-    def calculate_surround_lives(board: List[List[int]], row: int, column: int) -> int:
+    def calculate_surround_lives(
+        self, board: List[List[int]], row: int, column: int
+    ) -> int:
         rows = len(board)
         columns = len(board[0])
         surround_indexes = [
@@ -29,6 +38,6 @@ class Solution:
         ]
         result = 0
         for i, j in surround_indexes:
-            if 0 <= i < rows and 0 <= j < column:
+            if 0 <= i < rows and 0 <= j < columns:
                 result += board[i][j]
         return result
