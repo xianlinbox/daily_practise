fb3a3d15edb2cf7a6c8d579cbd7353846f5f6b48
xianning liu <xianlinbox@gmail.com>
2024-05-04 09:38:52 -0500

    add set matrix zero

---
+++ leet_code/set_matrix_0s/solution.py
@@ -0,0 +1,10 @@
+from typing import List
+
+# https://leetcode.com/problems/set-matrix-zeroes/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
---
+++ leet_code/set_matrix_0s/test_solution.py
@@ -0,0 +1,22 @@
+import unittest
+from solution import Solution
+
+
+class TestJump(unittest.TestCase):
+    solution = Solution()
+
+    def test_case_0(self):
+        matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+        result = self.solution.setZeroes(matrix)
+        expected = [[1, 0, 1], [0, 0, 0], [1, 0, 1]]
+        self.assertEqual(expected, result)
+
+    def test_case_1(self):
+        matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
+        result = self.solution.setZeroes(matrix)
+        expected = [[0, 0, 0, 0], [0, 4, 5, 0], [0, 3, 1, 0]]
+        self.assertEqual(expected, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
