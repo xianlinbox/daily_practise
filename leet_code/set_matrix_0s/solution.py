2c98572e1e8434455f82d9d912b0b19dbd71a836
xianning liu <xianlinbox@gmail.com>
2024-05-05 08:19:15 -0500

    first solution

--- leet_code/set_matrix_0s/solution.py
+++ leet_code/set_matrix_0s/solution.py
@@ -5,6 +5,17 @@ from typing import List
 
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
+        rows = len(matrix)
+        columns = len(matrix[0])
+        zero_rows = set()
+        zero_columns = set()
+        for row in range(rows):
+            for column in range(columns):
+                if matrix[row][column] == 0:
+                    zero_rows.add(row)
+                    zero_columns.add(column)
+
+        for row in range(rows):
+            for column in range(columns):
+                if row in zero_rows or column in zero_columns:
+                    matrix[row][column] = 0
--- leet_code/set_matrix_0s/test_solution.py
+++ leet_code/set_matrix_0s/test_solution.py
@@ -7,15 +7,15 @@ class TestJump(unittest.TestCase):
 
     def test_case_0(self):
         matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
-        result = self.solution.setZeroes(matrix)
+        self.solution.setZeroes(matrix)
         expected = [[1, 0, 1], [0, 0, 0], [1, 0, 1]]
-        self.assertEqual(expected, result)
+        self.assertEqual(expected, matrix)
 
     def test_case_1(self):
         matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
-        result = self.solution.setZeroes(matrix)
+        self.solution.setZeroes(matrix)
         expected = [[0, 0, 0, 0], [0, 4, 5, 0], [0, 3, 1, 0]]
-        self.assertEqual(expected, result)
+        self.assertEqual(expected, matrix)
 
 
 if __name__ == "__main__":
