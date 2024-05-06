285d9d438b3ea6cfe4b59c5e1f918cf8a0699adc
xianning liu <xianlinbox@gmail.com>
2024-05-06 09:48:22 -0500

    add spiral matrix

---
+++ leet_code/spiral_matrix/solution.py
@@ -0,0 +1,8 @@
+from typing import List
+
+# https://leetcode.com/problems/spiral-matrix/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        pass
---
+++ leet_code/spiral_matrix/test_solution.py
@@ -0,0 +1,22 @@
+import unittest
+from solution import Solution
+
+
+class TestJump(unittest.TestCase):
+    solution = Solution()
+
+    def test_case_0(self):
+        matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        result = self.solution.spiralOrder(matrix)
+        expected = [1, 2, 3, 6, 9, 8, 7, 4, 5]
+        self.assertEqual(expected, result)
+
+    def test_case_1(self):
+        matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+        result = self.solution.spiralOrder(matrix)
+        expected = [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+        self.assertEqual(expected, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
