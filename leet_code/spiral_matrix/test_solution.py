04ff652a9ae437797015c82638cd480314170fff
xianning liu <xianlinbox@gmail.com>
2024-05-06 17:38:18 -0500

    remove comments on test

--- leet_code/spiral_matrix/test_solution.py
+++ leet_code/spiral_matrix/test_solution.py
@@ -5,11 +5,11 @@ from solution import Solution
 class TestJump(unittest.TestCase):
     solution = Solution()
 
-    # def test_case_0(self):
-    #     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    #     result = self.solution.spiralOrder(matrix)
-    #     expected = [1, 2, 3, 6, 9, 8, 7, 4, 5]
-    #     self.assertEqual(expected, result)
+    def test_case_0(self):
+        matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        result = self.solution.spiralOrder(matrix)
+        expected = [1, 2, 3, 6, 9, 8, 7, 4, 5]
+        self.assertEqual(expected, result)
 
     def test_case_1(self):
         matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
