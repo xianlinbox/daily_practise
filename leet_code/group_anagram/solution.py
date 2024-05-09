58b7260142620a99f813d6539e011910d2c23de4
xianning liu <xianlinbox@gmail.com>
2024-05-09 14:05:28 -0500

    add first solution

--- leet_code/group_anagram/solution.py
+++ leet_code/group_anagram/solution.py
@@ -1,8 +1,18 @@
 from typing import List
 
-# https://leetcode.com/problems/set-matrix-zeroes/description/?envType=study-plan-v2&envId=top-interview-150
+# https://leetcode.com/problems/group-anagrams/description/?envType=study-plan-v2&envId=top-interview-150
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        pass
+        result: dict[tuple, List] = {}
+        for str in strs:
+            char_array = list(str)
+            char_array.sort()
+            key = tuple(char_array)
+
+            if key in result.keys():
+                result[key].append(str)
+            else:
+                result[key] = [str]
+        return list(result.values())
