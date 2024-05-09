004c9f346c6303aeb989b3753a6bfb2ee2815f51
xianning liu <xianlinbox@gmail.com>
2024-05-09 13:46:52 -0500

    add group anagram

---
+++ leet_code/group_anagram/solution.py
@@ -0,0 +1,8 @@
+from typing import List
+
+# https://leetcode.com/problems/set-matrix-zeroes/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        pass
---
+++ leet_code/group_anagram/test_solution.py
@@ -0,0 +1,28 @@
+import unittest
+from solution import Solution
+
+
+class TestJump(unittest.TestCase):
+    solution = Solution()
+
+    def test_case_0(self):
+        strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+        result = self.solution.groupAnagrams(strs)
+        expected = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+        self.assertEqual(expected, result)
+
+    def test_case_1(self):
+        strs = [""]
+        result = self.solution.groupAnagrams(strs)
+        expected = [[""]]
+        self.assertEqual(expected, result)
+
+    def test_case_2(self):
+        strs = ["a"]
+        result = self.solution.groupAnagrams(strs)
+        expected = [["a"]]
+        self.assertEqual(expected, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
