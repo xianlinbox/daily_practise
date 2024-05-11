d791bc0f3177070a798814ecd688165fe58e86cb
xianning liu <xianlinbox@gmail.com>
2024-05-11 09:35:55 -0500

    add test cases back

--- leet_code/longest_cons_seq/test_solution.py
+++ leet_code/longest_cons_seq/test_solution.py
@@ -5,15 +5,15 @@ from solution import Solution
 class TestJump(unittest.TestCase):
     solution = Solution()
 
-    # def test_case_0(self):
-    #     nums = [100, 4, 200, 1, 3, 2]
-    #     result = self.solution.longestConsecutive(nums)
-    #     self.assertEqual(4, result)
+    def test_case_0(self):
+        nums = [100, 4, 200, 1, 3, 2]
+        result = self.solution.longestConsecutive(nums)
+        self.assertEqual(4, result)
 
-    # def test_case_1(self):
-    #     nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
-    #     result = self.solution.longestConsecutive(nums)
-    #     self.assertEqual(9, result)
+    def test_case_1(self):
+        nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
+        result = self.solution.longestConsecutive(nums)
+        self.assertEqual(9, result)
 
     def test_case_2(self):
         nums = [1, 2, 0, 1]
