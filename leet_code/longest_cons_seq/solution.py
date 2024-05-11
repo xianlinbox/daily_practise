f825682b598280ff2c5e7a5f19621b2d2a413913
xianning liu <xianlinbox@gmail.com>
2024-05-10 19:20:07 -0500

    first solution

--- leet_code/longest_cons_seq/solution.py
+++ leet_code/longest_cons_seq/solution.py
@@ -5,4 +5,14 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        pass
+        nums.sort()
+        number_count = len(nums)
+        start_point = 0
+        seq_length = -1
+        for i in range(number_count - 1):
+            print(start_point, seq_length)
+            if nums[i + 1] != nums[i] + 1:
+                seq_length = max(i - start_point + 1, seq_length)
+                start_point = i + 1
+
+        return max(seq_length, number_count - start_point)
