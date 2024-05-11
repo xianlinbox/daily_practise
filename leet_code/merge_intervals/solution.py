2338145c932f09dbc2455adffab68b7b4ae95ca3
xianning liu <xianlinbox@gmail.com>
2024-05-11 09:35:36 -0500

    add merge intervals

---
+++ leet_code/merge_intervals/solution.py
@@ -0,0 +1,8 @@
+from typing import List
+
+# https://leetcode.com/problems/merge-intervals/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        pass
---
+++ leet_code/merge_intervals/test_solution.py
@@ -0,0 +1,22 @@
+import unittest
+from solution import Solution
+
+
+class TestJump(unittest.TestCase):
+    solution = Solution()
+
+    def test_case_0(self):
+        intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+        result = self.solution.merge(intervals)
+        expected = [[1, 6], [8, 10], [15, 18]]
+        self.assertEqual(expected, result)
+
+    def test_case_1(self):
+        intervals = [[1, 4], [4, 5]]
+        result = self.solution.merge(intervals)
+        expected = [[1, 5]]
+        self.assertEqual(expected, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
