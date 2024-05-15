fa03c4441a9b24ef11d45d4b25d03b6a9f6ec395
xianning liu <xianlinbox@gmail.com>
2024-05-15 09:54:39 -0500

    add solution

--- leet_code/sim_path/solution.py
+++ leet_code/sim_path/solution.py
@@ -1,8 +1,24 @@
 from typing import List
 
-# https://leetcode.com/problems/merge-intervals/description/?envType=study-plan-v2&envId=top-interview-150
+# https://leetcode.com/problems/simplify-path/?envType=study-plan-v2&envId=top-interview-150
 
 
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        return ""
+        char_list = path.split("/")
+        result = []
+        for c in char_list:
+            match c:
+                case "..":
+                    result.pop()
+                case ".":
+                    continue
+                case "":
+                    result.append("/")
+                case _:
+                    if result and result[-1] == "/":
+                        result.pop()
+                    result.append("/" + c)
+        if len(result) > 1 and result[-1] == "/":
+            result.pop()
+        return "".join(result)
