1cd8438d1e4751f33c77927cf99be2dfc5fccf74
xianning liu <xianlinbox@gmail.com>
2024-05-15 08:58:03 -0500

    add simplify path problem

---
+++ leet_code/sim_path/solution.py
@@ -0,0 +1,8 @@
+from typing import List
+
+# https://leetcode.com/problems/merge-intervals/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        return ""
---
+++ leet_code/sim_path/test_solution.py
@@ -0,0 +1,40 @@
+import unittest
+from solution import Solution
+
+
+class TestJump(unittest.TestCase):
+    solution = Solution()
+
+    def test_case_0(self):
+        path = "/home/"
+        result = self.solution.simplifyPath(path)
+        expected = "/home"
+        self.assertEqual(expected, result)
+
+    def test_case_1(self):
+        path = "/home//foo/"
+        result = self.solution.simplifyPath(path)
+        expected = "/home/foo"
+        self.assertEqual(expected, result)
+
+    def test_case_2(self):
+        path = "/home/user/Documents/../Pictures"
+        result = self.solution.simplifyPath(path)
+        expected = "/home/user/Pictures"
+        self.assertEqual(expected, result)
+
+    def test_case_3(self):
+        path = "/../"
+        result = self.solution.simplifyPath(path)
+        expected = "/"
+        self.assertEqual(expected, result)
+
+    def test_case_4(self):
+        path = "/.../a/../b/c/../d/./"
+        result = self.solution.simplifyPath(path)
+        expected = "/.../b/d"
+        self.assertEqual(expected, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
