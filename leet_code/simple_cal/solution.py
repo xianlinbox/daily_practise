4061e2a4a4f94c25aa390c4f31d3cc4913f22f3e
xianning liu <xianlinbox@gmail.com>
2024-06-16 19:46:05 -0500

    calculate multiple number'

--- leet_code/simple_cal/solution.py
+++ leet_code/simple_cal/solution.py
@@ -1,8 +1,39 @@
 from typing import List
 
-# https://leetcode.com/problems/min-stack/description/?envType=study-plan-v2&envId=top-interview-150
+# https://leetcode.com/problems/basic-calculator/?envType=study-plan-v2&envId=top-interview-150
 
 
 class Solution:
     def calculate(self, s: str) -> int:
-        pass
+        operators = []
+        numbers = []
+        for char in s:
+            match char:
+                case ' ':
+                    continue
+                case '+' | '-':
+                    if len(numbers) >= 2:
+                        op = operators.pop()
+                        numbers.append(self.calcaulte(numbers, op))
+                    operators.append(char)
+                case '(':
+                    operators.append(char)
+                case ')':
+                    curr_op = operators.pop()
+                    while  curr_op != '(':
+                        self.calcaulte(numbers, curr_op)
+                        curr_op = operators.pop()
+                case _:
+                    numbers.append(int(char))
+        
+        return self.calcaulte(numbers, operators.pop())
+    
+    def calcaulte(self, numbers, op) -> str:
+        number_1 = numbers.pop()
+        number_2 = numbers.pop()
+        if op == '+':
+           return number_2+ number_1
+        else:
+            return number_2 - number_1 
+    
+
